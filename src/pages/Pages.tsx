bfcf690975441819f77712081fdd7f61a2ebad00
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-03-26 21:20:31 +0100

    add lazy loading

--- src/pages/Pages.tsx
+++ src/pages/Pages.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './home/Home';
-import Contact from './contact/Contact';
 
+import Loading from '../loading/Loading';
+
+const Contact = React.lazy(() => import('./contact/Contact'));
 export interface PagesProps {}
 
 const Pages: React.SFC<PagesProps> = () => (
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/contact" exact component={Contact} />
-  </Switch>
+  <Suspense fallback={<Loading />}>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/contact" exact component={Contact} />
+    </Switch>
+  </Suspense>
 );
 
 export default Pages;
