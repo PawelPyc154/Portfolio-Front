26393eb6de250cc25465795cc3c0d6c41fafd129
PawelPyc154 <pawelpyc154@gmail.comnpm>
2020-04-07 01:55:32 +0200

    contactform add request and animation

--- src/pages/contact/ContactForm.tsx
+++ src/pages/contact/ContactForm.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Formik, Form } from 'formik';
 import * as yup from 'yup';
 import './ContactForm.scss';
 import { motion } from 'framer-motion';
+import axios from 'axios';
 
 export interface ContactFormProps {}
 
 const ContactForm: React.SFC<ContactFormProps> = () => {
-  const validationSchema = yup.object({
-    email: yup
-      .string()
-      .email('Invalid email')
-      .required('Required'),
+  const [animateVariant, setAnimateVariant] = useState('hidden');
+  useEffect(() => {
+    setAnimateVariant('visible');
+  }, []);
 
-    message: yup
-      .string()
-      .required('Required')
-      .min(6, 'Too Short!'),
+  const validationSchema = yup.object({
+    email: yup.string().email('Invalid email').required('Required'),
+    subject: yup.string().required('Required').min(6, 'Too Short!').max(50, 'Too Long!'),
+    message: yup.string().required('Required').min(6, 'Too Short!').max(500, 'Too Long!'),
   });
+
+  const handleSubmit = async (
+    email: string,
+    subject: string,
+    message: string,
+    resetForm: () => void,
+  ) => {
+    try {
+      await axios.post('https://portfolio321123.herokuapp.com/email', { email, subject, message });
+
+      await setAnimateVariant('hidden');
+      await resetForm();
+      await setTimeout(() => {
+        setAnimateVariant('visible');
+      }, 1400);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Formik
@@ -25,14 +44,13 @@ const ContactForm: React.SFC<ContactFormProps> = () => {
       validationSchema={validationSchema}
       initialValues={{
         email: '',
+        subject: '',
         message: '',
       }}
-      onSubmit={({ email, message }, { setSubmitting }) => {
-        setSubmitting(true);
-        console.log(email);
-        console.log(message);
-
-        setSubmitting(false);
+      onSubmit={async ({ email, subject, message }, { setSubmitting, resetForm }) => {
+        await setSubmitting(true);
+        await handleSubmit(email, subject, message, resetForm);
+        await setSubmitting(false);
       }}
     >
       {({ values, errors, touched, handleChange, handleBlur, isSubmitting, isValid }) => (
@@ -47,10 +65,39 @@ const ContactForm: React.SFC<ContactFormProps> = () => {
             onBlur={handleBlur}
             value={values.email}
             placeholder="Email"
-            animate={{ x: [100, 0], opacity: [0, 1] }}
-            transition={{ delay: 0.4 }}
+            // animate={{ x: [100, 0], opacity: [0, 1] }}
+            initial="hidden"
+            animate={animateVariant}
+            variants={{
+              visible: { x: 0, opacity: 1, transition: { delay: 0.6, duration: 0.8 } },
+              hidden: { x: 100, opacity: 0, transition: { delay: 0.6, duration: 0.8 } },
+            }}
           />
-          {errors.message && touched.message && <div className="Form__validation">{errors.message}</div>}
+
+          {errors.subject && touched.subject && (
+            <div className="Form__validation">{errors.subject}</div>
+          )}
+          <motion.input
+            style={errors.subject && touched.subject ? { border: '1px solid #e74c3c' } : {}}
+            className="Form__input"
+            type="subject"
+            name="subject"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.subject}
+            placeholder="Temat"
+            // animate={{ x: [100, 0], opacity: [0, 1] }}
+            // transition={{ delay: 0.4 }}
+            initial="hidden"
+            animate={animateVariant}
+            variants={{
+              visible: { x: 0, opacity: 1, transition: { delay: 0.4, duration: 0.8 } },
+              hidden: { x: 100, opacity: 0, transition: { delay: 0.4, duration: 0.8 } },
+            }}
+          />
+          {errors.message && touched.message && (
+            <div className="Form__validation">{errors.message}</div>
+          )}
           <motion.textarea
             style={errors.message && touched.message ? { border: '1px solid #e74c3c' } : {}}
             className="Form__input Form__textarea"
@@ -59,16 +106,35 @@ const ContactForm: React.SFC<ContactFormProps> = () => {
             onBlur={handleBlur}
             value={values.message}
             placeholder="Wiadomość"
-            animate={{ x: [100, 0], opacity: [0, 1] }}
-            transition={{ delay: 0.2 }}
+            // animate={{ x: [100, 0], opacity: [0, 1] }}
+            // transition={{ delay: 0.2 }}
+            initial="hidden"
+            animate={animateVariant}
+            variants={{
+              visible: { x: 0, opacity: 1, transition: { delay: 0.2, duration: 0.8 } },
+              hidden: { x: 100, opacity: 0, transition: { delay: 0.2, duration: 0.8 } },
+            }}
           />
           <motion.button
-            style={isValid && values.email && values.message ? { border: '1px solid #2ecc71' } : {}}
+            style={
+              isValid && values.email && values.subject && values.message
+                ? {
+                    border: '1px solid #2ecc71',
+                    backgroundColor: isSubmitting ? '#2ecc71' : 'transparent',
+                  }
+                : {}
+            }
             disabled={isValid && isSubmitting}
             type="submit"
             className="Form__btn"
-            animate={{ x: [100, 0], opacity: [0, 1] }}
-            transition={{ delay: 0 }}
+            // animate={{ x: [100, 0], opacity: [0, 1] }}
+            // transition={{ delay: 0 }}
+            initial="hidden"
+            animate={animateVariant}
+            variants={{
+              visible: { x: 0, opacity: 1, transition: { delay: 0, duration: 0.8 } },
+              hidden: { x: 100, opacity: 0, transition: { delay: 0, duration: 0.8 } },
+            }}
           >
             Wyślij
           </motion.button>
